9afafb04e89c6572656951ff5f1b267dcb328c04
Dr Nic Williams <drnicwilliams@gmail.com>
2012-08-05 11:29:58 -0600

    Better user/groups cleanup & a puma test

--- test/git-user-test.sh
+++ test/git-user-test.sh
@@ -13,28 +13,36 @@ release_path=$(pwd)
 rm -rf /tmp/before_all_run_already
 
 before_all() {
-  echo "|"
-  echo "| Stopping any existing jobs"
-  echo "|"
   sm bosh-solo stop
 
-  echo "|"
-  echo "| Deleting storage"
-  echo "|"
   rm -rf /var/vcap/store
 
-  if [ -d /home/git/ ]
+  if [ "$(cat /etc/passwd | grep '^git:')" != '' ]
   then
-    echo "|"
-    echo "| Deleting git user"
-    echo "|"
     userdel -f git
-    rm -rf /home/git/
   fi
+  if [ "$(cat /etc/group | grep '^git:')" != '' ]
+  then
+    delgroup git
+  fi
+  rm -rf /home/git/
+
+  if [ "$(cat /etc/passwd | grep '^gitlab:')" != '' ]
+  then
+    userdel -f gitlab
+  fi
+  if [ "$(cat /etc/group | grep '^gitlab:')" != '' ]
+  then
+    delgroup gitlab
+  fi
+  rm -rf /home/gitlab/
 
   # update deployment with example properties
   example=${release_path}/examples/one_user.yml
   sm bosh-solo update ${example}
+
+  # wait for everything to boot up, migrate, create users, etc
+  sleep 20
 
   # show last 20 processes (for debugging if test fails)
   ps ax | tail -n 20
@@ -66,7 +74,7 @@ it_has_chunkin_module_in_nginx() {
   test $(/var/vcap/packages/nginx_next/sbin/nginx -V 2>&1 | grep 'chunkin' | wc -l) = 1
 }
 
-it_runs_nginx() {
+it_runs_gitolite_nginx() {
   expected='sbin/nginx -c /var/vcap/jobs/gitolite/config/nginx.conf'
   test $(ps ax | grep "${expected}" | grep -v 'grep' | wc -l) = 1
 }
@@ -74,5 +82,14 @@ it_runs_nginx() {
 it_restarted_nginx() {
   test $(cat /var/vcap/sys/log/gitolite/nginx.stderr.log | wc -l) = 0
 }
-# TODO test that .ssh/authorized_keys uses symlinks instead of 
-# command="/var/vcap/data/packages/gitolite/0.10-dev/src/gitolite-shell gitolite
+
+it_runs_gitlabhq_using_puma() {
+  expected='puma --pidfile /var/vcap/sys/run/gitlabhq/gitlabhq.pid -p 5000 -t 0:16'
+  test $(ps ax | grep "${expected}" | grep -v 'grep' | wc -l) = 1
+}
+
+it_runs_gitlabhq_nginx() {
+  expected='sbin/nginx -c /var/vcap/jobs/gitlabhq/config/nginx.conf'
+  test $(ps ax | grep "${expected}" | grep -v 'grep' | wc -l) = 1
+}
+

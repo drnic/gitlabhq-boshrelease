89a95e68bdd51eab903e4a9a7655a99e570f8e80
Dr Nic Williams <drnicwilliams@gmail.com>
2012-08-12 06:56:03 -0600

    set git's init.templatedir

--- jobs/postgres/templates/helpers/ctl_setup.sh
+++ jobs/postgres/templates/helpers/ctl_setup.sh
@@ -64,6 +64,13 @@ then
   export PYTHONPATH=$WEBAPP_DIR/vendor/lib/python
 fi
 
+if [[ -d /var/vcap/packages/git ]]
+then
+  # templatedir set to explicit, possibly incorrect: 
+  # /var/vcap/data/packages/git/0.1-dev/share/git-core/templatesroot
+  git config --global init.templatedir /var/vcap/packages/git/share/git-core/templatesroot
+fi  
+
 if [[ -d /var/vcap/packages/java7 ]]
 then
   export JAVA_HOME="/var/vcap/packages/java7"

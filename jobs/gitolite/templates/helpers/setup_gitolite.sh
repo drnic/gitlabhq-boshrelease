1dd0191324c15139d70acec5ffc6eda15ae767d4
Dr Nic Williams <drnicwilliams@gmail.com>
2012-08-12 21:44:35 -0600

    git_user_grp is a locally used variable

--- jobs/gitolite/templates/helpers/setup_gitolite.sh
+++ jobs/gitolite/templates/helpers/setup_gitolite.sh
@@ -10,6 +10,7 @@ set +x
 
 export HOME=${GIT_HOME}
 packages=/var/vcap/packages
+export git_user_grp=${GIT_USER}:vcap
 
 if [[ ! -d ${GIT_HOME} ]]
 then
@@ -22,8 +23,8 @@ then
       --disabled-password \
       --home ${GIT_HOME} ${GIT_USER}
 
-  chpst -u ${GIT_USER_GRP} touch ${GIT_HOME}/.bashrc
-  chpst -u ${GIT_USER_GRP} git config --global \
+  chpst -u ${git_user_grp} touch ${GIT_HOME}/.bashrc
+  chpst -u ${git_user_grp} git config --global \
     init.templatedir /var/vcap/packages/git/share/git-core/templatesroot
   
   echo 'export PATH=${packages}/perl/bin:$PATH' >> ${GIT_HOME}/.bashrc
@@ -32,7 +33,7 @@ then
 fi
 
 mkdir -p ${GIT_HOME}/repositories/
-chown ${GIT_USER_GRP} ${GIT_HOME}/repositories/
+chown ${git_user_grp} ${GIT_HOME}/repositories/
 chmod 775 ${GIT_HOME}/repositories/
 
 link_job_file config/dot.gitolite.rc $GIT_HOME/.gitolite.rc
@@ -42,13 +43,13 @@ export PATH=${packages}/git/bin:$PATH
 export PATH=${packages}/gitolite/src:$PATH
 
 chown git -R $(readlink ${packages}/gitolite)
-echo chpst -u ${GIT_USER_GRP} $(readlink ${packages}/gitolite)/install
-chpst -u ${GIT_USER_GRP} $(readlink ${packages}/gitolite)/install
+echo chpst -u ${git_user_grp} $(readlink ${packages}/gitolite)/install
+chpst -u ${git_user_grp} $(readlink ${packages}/gitolite)/install
 
 echo "${ADMIN_PUB_KEY}" > ${GIT_HOME}/${GIT_USER}.pub
-chown ${GIT_USER_GRP} ${GIT_HOME}/${GIT_USER}.pub
+chown ${git_user_grp} ${GIT_HOME}/${GIT_USER}.pub
 
 # TODO how to make idempotent? what is it doing?
 echo "gitolite at" $(which gitolite)
-echo chpst -u ${GIT_USER_GRP} gitolite setup -pk ${GIT_HOME}/${GIT_USER}.pub
-chpst -u ${GIT_USER_GRP} gitolite setup -pk ${GIT_HOME}/${GIT_USER}.pub
+echo chpst -u ${git_user_grp} gitolite setup -pk ${GIT_HOME}/${GIT_USER}.pub
+chpst -u ${git_user_grp} gitolite setup -pk ${GIT_HOME}/${GIT_USER}.pub
